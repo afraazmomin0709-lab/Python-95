07a938ff8609dc0235aa4b735311d73542d5ada9
Afraaz Momin <afraazmomin0709@gmail.com>
2026-07-28 13:28:12 +0000

    commit

---
+++ traffic_signal.py
@@ -0,0 +1,9 @@
+color = str(input("Enter the color of traffic signal : "))
+if (color == "red"):
+    print("Stop")
+elif(color == "yellow"):
+    print("Slow down/Ready")
+elif(color == "green"):
+    print("Go")
+else:
+    print("Invalid color")
